2f054ef42277c78e5fb4a5418cc2f953b2eb0b7f
liweiwei1419 <121088825@qq.com>
2022-10-27 09:51:42 +0800

    实现了基本的翻页。

--- main.py
+++ main.py
@@ -7,31 +7,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.common.keys import Keys
+
 import time
 
 
-def click_baozhi_tab():
-    baozhi_a = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/ul[1]/li[4]/a')
-    baozhi_a.click()
-
-
-if __name__ == '__main__':
-    # 浏览器不关闭 begin
+def driver_init():
+    global driver
+    # 设置浏览器不关闭 begin
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
-    # 浏览器不关闭 end
+    # 设置浏览器不关闭 end
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # 全局设置
     driver.implicitly_wait(5)
 
-    # todo 需要登录
 
-    root_url = 'https://www.cnki.net/'
-    driver.get(url=root_url)
-
+def login():
     login_btn = driver.find_element(By.XPATH, '//*[@id="Ecp_top_login"]/a')
     login_btn.click()
-
     time.sleep(1)
 
     user_input = driver.find_element(By.XPATH, '//*[@id="Ecp_TextBoxUserName"]')
@@ -43,20 +37,32 @@ if __name__ == '__main__':
     login_submit_btn = driver.find_element(By.XPATH, '//*[@id="Ecp_Button1"]')
     login_submit_btn.click()
 
-    # 窗口最大化
-    # driver.maximize_window()
+
+def click_newspaper_tab():
+    newspaper_a = driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/ul[1]/li[4]/a')
+    newspaper_a.click()
+
+
+if __name__ == '__main__':
+    driver_init()
+
+    root_url = 'https://www.cnki.net/'
+    driver.get(url=root_url)
+
+    login()
+
     # 找到「高级搜索」按钮，并点击
     high_search_button = driver.find_element(By.XPATH, '//*[@id="highSearch"]')
     high_search_button.click()
     # 切换到最新打开的窗口
-    handles = driver.window_handles
-    search_page = handles[-1]
-    driver.switch_to.window(search_page)
-    # 获取 WebDriver 对象当前聚焦的浏览器窗口句柄
-    # print(driver.current_url)
-    # 第 1 步：点击「报纸」选项卡
-    click_baozhi_tab()
 
+    # 搜索主页，这一页很重要
+    search_page = driver.window_handles[-1]
+    driver.switch_to.window(search_page)
+
+    # 第 1 步：点击「报纸」选项卡
+    click_newspaper_tab()
+    # 这一步很重要，要让页面加载完
     time.sleep(1)
 
     # 第 2 步：选项卡 1 选择「全文」
@@ -64,30 +70,24 @@ if __name__ == '__main__':
     js = 'document.querySelector("#gradetxt > dd:nth-child(2) > div.input-box > div.sort.reopt > ' \
          'div.sort-list").style.display="block" '
     driver.execute_script(js)
-    # show_list = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.XPATH, '//*[@id="gradetxt"]/dd[1]/div[2]/div[1]/div[1]')))
-    # show_list.click()
-
     time.sleep(1)
-    # # fix_me 这里容易找不到
-    # # quan_wen_a_link = driver.find_element(By.XPATH, '//*[@id="gradetxt"]/dd[1]/div[2]/div[1]/div[2]/ul/li[5]/a')
-    #
+
     quan_wen_a_link = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="gradetxt"]/dd[1]/div[2]/div[1]/div[2]/ul/li[5]/a')))
-    print("1选择了", quan_wen_a_link.text)
-    # # 需要点击两下
+    print("下拉框 1 选择了", quan_wen_a_link.text)
+    # 需要点击两下
     quan_wen_a_link.click()
     time.sleep(1)
     quan_wen_a_link.click()
 
     # 第 3 步：选项卡 2 选择「全文」
-    js2 = 'document.querySelector("#gradetxt > dd:nth-child(3) > div.input-box > div.sort.reopt > div.sort-list").style.display="block"'
+    js2 = 'document.querySelector("#gradetxt > dd:nth-child(3) > div.input-box > div.sort.reopt > ' \
+          'div.sort-list").style.display="block"'
     driver.execute_script(js2)
     time.sleep(1)
 
     quan_wen_a_link2 = driver.find_element(By.XPATH, '//*[@id="gradetxt"]/dd[2]/div[2]/div[1]/div[2]/ul/li[5]/a')
-
-    print("2选择了", quan_wen_a_link2.text)
+    print("下拉框 2 选择了", quan_wen_a_link2.text)
     quan_wen_a_link2.click()
     time.sleep(1)
     quan_wen_a_link2.click()
@@ -108,6 +108,35 @@ if __name__ == '__main__':
 
     rows = driver.find_elements(By.XPATH, '//*[@id="gridTable"]/table/tbody/tr')
     print(len(rows))
+
+    js3 = 'document.querySelector("#perPageDiv > ul").style.display="block" '
+    driver.execute_script(js3)
+
+    page_size = driver.find_element(By.XPATH, '//*[@id="perPageDiv"]/ul/li[3]/a')
+
+    time.sleep(2)
+
+    page_size.click()
+
+    time.sleep(2)
+
+
+    total_pages_str = driver.find_element(By.XPATH, '//*[@id="countPageDiv"]/span[2]')
+    print("总页数", total_pages_str.text.split('/')[1])
+    total_pages = int(total_pages_str.text.split('/')[1])
+
+    # 12
+    i = 1
+    while True:
+        print('爬第 ' + str(i) + ' 页的内容')
+        time.sleep(1)
+        i += 1
+
+        next_page_btn = driver.find_elements(By.XPATH, '//*[@id="Page_next_top"]')
+        if len(next_page_btn) == 1:
+            driver.execute_script("arguments[0].click();", next_page_btn[0])
+        else:
+            break
 
     # for row in rows:
     #     article_url = row.find_element(By.CSS_SELECTOR, "td.name > a")
